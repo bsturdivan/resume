e98d8378b968368aabe5bad6d210e0436a7919c3
Brian Sturdivan <bsturdivan@gmail.com>
2024-04-09 22:34:14 -0700

    update generator script

--- scripts/generate.js
+++ scripts/generate.js
@@ -2,14 +2,17 @@ const fs = require('fs')
 
 const [_, __, ...vars] = process.argv
 const generator = vars[0] === 'cover' ? 'cover-letters' : 'resumes'
-const file = vars[1]
+const file = process.env.npm_config_file
 
 const { mdToPdf } = require('md-to-pdf');
 
 (async () => {
-	const pdf = await mdToPdf({ path: `${generator}/${file}.md` }, { dest: `bin/${generator}/${file}.pdf` })
+	try {
+		const pdf = await mdToPdf({ path: `${generator}/${file}.md` }, { dest: `dist/${generator}/${file}.pdf` })
+		fs.writeFileSync(pdf.filename, pdf.content)
 
-	if (pdf) {
-		fs.writeFileSync(pdf.filename, pdf.content);
+		console.log(`File written to dist/${generator}/${file}.pdf`)
+	} catch(error) {
+		console.error(error)
 	}
 })()
